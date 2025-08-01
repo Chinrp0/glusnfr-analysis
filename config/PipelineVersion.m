9ea13b1f0537cfc682103cb5f43e44804904a07b
Chin Patel <ChinRP0@gmail.com>
2025-08-01 00:22:11 -0400

    Automatic legacy version

--- config/PipelineVersion.m
+++ config/PipelineVersion.m
@@ -4,11 +4,12 @@ function version_info = PipelineVersion()
     % Returns version information for the entire pipeline
     
     version_info = struct();
-    version_info.version = '56.2.1';  % Semantic versioning: MAJOR.MINOR.PATCH
+    version_info.version = '57.0.0';  % Semantic versioning: MAJOR.MINOR.PATCH
     version_info.build_date = '2025-07-31';
     version_info.matlab_version_required = 'R2019a';
     version_info.version_name = 'Modular Release';
     
-    % Legacy version number for compatibility
-    version_info.legacy_version = '56-2';
+    % Automatic legacy version number for compatibility
+    version_parts = strsplit(version_info.version, '.');
+    version_info.legacy_version = strjoin(version_parts, '-');
 end
